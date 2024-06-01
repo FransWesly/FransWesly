f8cd84aa290d047c7351d46fe85a730fef6e6f3f
Gunawan <franswesly>
2024-06-02 02:53:52 +0900

    Pengarsipan

---
+++ arsip/kp/project_arsip_digital.sql
@@ -0,0 +1,267 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Waktu pembuatan: 09 Jan 2024 pada 01.59
+-- Versi server: 10.4.24-MariaDB
+-- Versi PHP: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `project_arsip_digital`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `admin`
+--
+
+CREATE TABLE `admin` (
+  `admin_id` int(11) NOT NULL,
+  `admin_nama` varchar(255) NOT NULL,
+  `admin_username` varchar(255) NOT NULL,
+  `admin_password` varchar(255) NOT NULL,
+  `admin_foto` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data untuk tabel `admin`
+--
+
+INSERT INTO `admin` (`admin_id`, `admin_nama`, `admin_username`, `admin_password`, `admin_foto`) VALUES
+(1, 'Frans Wesly Karoba', 'admin', '0192023a7bbd73250516f069df18b500', '1471275613_Screen Shot 2019-10-11 at 16.26.42.png');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `arsip`
+--
+
+CREATE TABLE `arsip` (
+  `arsip_id` int(11) NOT NULL,
+  `arsip_waktu_upload` datetime NOT NULL,
+  `arsip_petugas` int(11) NOT NULL,
+  `arsip_kode` varchar(255) NOT NULL,
+  `arsip_nama` varchar(255) NOT NULL,
+  `arsip_jenis` varchar(255) NOT NULL,
+  `arsip_kategori` int(11) NOT NULL,
+  `arsip_keterangan` text NOT NULL,
+  `arsip_file` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data untuk tabel `arsip`
+--
+
+INSERT INTO `arsip` (`arsip_id`, `arsip_waktu_upload`, `arsip_petugas`, `arsip_kode`, `arsip_nama`, `arsip_jenis`, `arsip_kategori`, `arsip_keterangan`, `arsip_file`) VALUES
+(2, '2019-10-10 15:09:59', 4, 'ARSIP-MN-0002', 'File keberngkatan', 'png', 4, 'tes ttead', '1162363338_Screen Shot 2019-10-10 at 13.22.15.png'),
+(3, '2019-10-10 16:02:44', 4, 'asd', 'asdasd 2x', 'pdf', 3, 'asdasd', '432536246_mamunur.pdf'),
+(4, '2019-10-12 17:02:16', 5, 'MN-002', 'Contoh Surat Izin Pelaksanaan', 'pdf', 4, 'Ini Contoh Surat Izin Pelaksanaan', '1352467019_c4611_sample_explain.pdf'),
+(5, '2019-10-12 17:03:01', 5, 'MN-003', 'Contoh Keputusan Kerja', 'pdf', 3, 'Contoh Keputusan Kerja pegawai', '1765932248_Contoh-surat-lamaran-kerja-pdf (1).pdf'),
+(6, '2019-10-12 17:03:37', 5, 'MN-004', 'Contoh Surat Izin Pegawai', 'pdf', 7, 'berikut Contoh Surat Izin Pegawai untuk pelaksana kerja', '1651167980_instructions-for-adding-your-logo.pdf'),
+(7, '2019-10-12 17:04:30', 5, 'MN-005', 'Contoh SPK Proyek Kontraktor', 'pdf', 5, 'Contoh SPK Proyek Kontraktor adalah contoh surat SPK KONTRAK', '142845393_OoPdfFormExample.pdf'),
+(8, '2019-10-12 17:05:22', 5, 'MN-006', 'SPK Kontrak Jembatan', 'pdf', 6, 'Surat SPK Kontrak Jembatan Layang', '106615077_sample-link_1.pdf'),
+(9, '2019-10-12 17:06:55', 6, 'MN-008', 'Contoh Curiculum Vitae Untuk Lamaran Kerja', 'pdf', 10, 'Contoh Curiculum Vitae Untuk Lamaran Kerja untuk pegawai baru', '927990343_pdf-sample(1).pdf'),
+(10, '2019-10-12 17:07:30', 6, 'MN-009', 'Surat Cuti Sakit Pegawai', 'pdf', 7, 'Contoh Surat Cuti Sakit Pegawai baru', '2071946811_PEMBUATAN FILE PDF_FNH_tamim (1).pdf'),
+(17, '2024-01-08 21:29:22', 4, '098', 'Uji03', 'pdf', 5, 'uji', '2098986335_432536246_mamunur.pdf'),
+(18, '2024-01-08 21:29:59', 4, '90877', 'Dokumen Sita', 'png', 8, 'ok', '1483508284_1162363338_Screen Shot 2019-10-10 at 13.22.15.png'),
+(19, '2024-01-08 21:30:35', 4, '0988', 'cong', 'pdf', 7, 'ok', '1842972130_142845393_OoPdfFormExample.pdf'),
+(20, '2024-01-08 21:33:17', 4, '564', 'Dokumen Sita', 'pdf', 5, 'ok', '1787768429_Nama-Nama Pengurus BEM Fikom Baru.pdf');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `kategori_id` int(11) NOT NULL,
+  `kategori_nama` varchar(255) NOT NULL,
+  `kategori_keterangan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data untuk tabel `kategori`
+--
+
+INSERT INTO `kategori` (`kategori_id`, `kategori_nama`, `kategori_keterangan`) VALUES
+(1, 'Tidak berkategori', 'Semua yang tidak memiliki kategori'),
+(3, 'Surat Keputusan', 'Format arsip untuk surat keputusan\r\n'),
+(4, 'Surat Izin Pelaksanaan', 'Contoh format surat izin pelaksaan pekerjaan'),
+(5, 'Surat Perintah Kerja Proyek jalan', 'Contoh format surat perintah untuk pekerjaan proyek jalan'),
+(6, 'Surat Perintah Kerja Proyek Jembatan', 'Contoh format untuk surat perintah kerja proyek jembatan'),
+(7, 'Surat Kesehatan Pegawai', 'Surat kesehatan untuk pegawai'),
+(8, 'Surat Lampiran Skripsi', 'Surat contoh lampiran untuk skripsi'),
+(10, 'Curiculum Vitae', 'Contoh format surat curiculum vitae untuk kenaikan jabatan');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `petugas`
+--
+
+CREATE TABLE `petugas` (
+  `petugas_id` int(11) NOT NULL,
+  `petugas_nama` varchar(255) NOT NULL,
+  `petugas_username` varchar(255) NOT NULL,
+  `petugas_password` varchar(255) NOT NULL,
+  `petugas_foto` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data untuk tabel `petugas`
+--
+
+INSERT INTO `petugas` (`petugas_id`, `petugas_nama`, `petugas_username`, `petugas_password`, `petugas_foto`) VALUES
+(4, 'Patrick Y.S Pinoa', 'petugas1', 'b53fe7751b37e40ff34d012c7774d65f', ''),
+(5, 'Junaidi Mus', 'petugas2', 'ac5604a8b8504d4ff5b842480df02e91', ''),
+(6, 'Jamilah Suanda', 'petugas3', '6f7dc121bccfd778744109cac9593474', '');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `riwayat`
+--
+
+CREATE TABLE `riwayat` (
+  `riwayat_id` int(11) NOT NULL,
+  `riwayat_waktu` datetime NOT NULL,
+  `riwayat_user` int(11) NOT NULL,
+  `riwayat_arsip` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data untuk tabel `riwayat`
+--
+
+INSERT INTO `riwayat` (`riwayat_id`, `riwayat_waktu`, `riwayat_user`, `riwayat_arsip`) VALUES
+(1, '2019-10-11 15:32:29', 8, 3),
+(2, '2019-10-12 17:09:31', 8, 10),
+(3, '2019-10-12 17:09:45', 8, 9),
+(4, '2019-10-12 17:09:50', 8, 8),
+(5, '2019-10-12 17:09:53', 8, 3),
+(6, '2019-10-12 17:10:07', 9, 10),
+(7, '2019-10-12 17:10:16', 9, 9),
+(8, '2019-10-12 17:10:19', 9, 8),
+(9, '2019-10-12 17:10:22', 9, 6),
+(10, '2019-10-12 17:10:26', 9, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `user_nama` varchar(100) NOT NULL,
+  `user_username` varchar(100) NOT NULL,
+  `user_password` varchar(100) NOT NULL,
+  `user_foto` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`user_id`, `user_nama`, `user_username`, `user_password`, `user_foto`) VALUES
+(8, 'Amon M.O Adii', 'user1', '24c9e15e52afc47c225b757e7bee1f9d', ''),
+(9, 'Reza Yuzanni', 'user2', '7e58d63b60197ceb55a1c487989a3720', ''),
+(10, 'Ajir Muhajier', 'user3', '92877af70a45fd6a2ed7fe81e1236b78', ''),
+(11, 'Cut Nanda Somay', 'user4', '3f02ebe3d7929b091e3d8ccfde2f3bc6', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indeks untuk tabel `arsip`
+--
+ALTER TABLE `arsip`
+  ADD PRIMARY KEY (`arsip_id`);
+
+--
+-- Indeks untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`kategori_id`);
+
+--
+-- Indeks untuk tabel `petugas`
+--
+ALTER TABLE `petugas`
+  ADD PRIMARY KEY (`petugas_id`);
+
+--
+-- Indeks untuk tabel `riwayat`
+--
+ALTER TABLE `riwayat`
+  ADD PRIMARY KEY (`riwayat_id`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `arsip`
+--
+ALTER TABLE `arsip`
+  MODIFY `arsip_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `kategori_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT untuk tabel `petugas`
+--
+ALTER TABLE `petugas`
+  MODIFY `petugas_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `riwayat`
+--
+ALTER TABLE `riwayat`
+  MODIFY `riwayat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
